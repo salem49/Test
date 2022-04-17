faf6179238984c8cce10eaebf73369623a53aff4
salem49 <salem.elmeryah@sesame.com.tn>
2022-04-17 06:18:20 +0200

    initial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -22,28 +22,28 @@ export class HeroService {
     private messageService: MessageService) { }
 
   /** GET heroes from the server */
-  // getHeroes(): Observable<Hero[]> {
-  //   return this.http.get<Hero[]>(this.heroesUrl)
-  //     .pipe(
-  //       tap(_ => this.log('fetched heroes')),
-  //       catchError(this.handleError<Hero[]>('getHeroes', []))
-  //     );
-  // }
+   getHeroes(): Observable<Hero[]> {
+     return this.http.get<Hero[]>(this.heroesUrl)
+       .pipe(
+         tap(_ => this.log('fetched heroes')),
+         catchError(this.handleError<Hero[]>('getHeroes', []))
+       );
+   }
 
 
   // /** GET hero by id. Return `undefined` when id not found */
-  //  getHeroNo404<Data>(id: number): Observable<Hero> {
-  //    const url = `${this.heroesUrl}/?id=${id}`;
-  //    return this.http.get<Hero[]>(url)
-  //      .pipe(
-  //        map(heroes => heroes[0]), // returns a {0|1} element array
-  //        tap(h => {
-  //          const outcome = h ? 'fetched' : 'did not find';
-  //         this.log(`${outcome} hero id=${id}`);
-  //        }),
-  //        catchError(this.handleError<Hero>(`getHero id=${id}`))
-  //     );
-  // }
+    getHeroNo404<Data>(id: number): Observable<Hero> {
+      const url = `${this.heroesUrl}/?id=${id}`;
+      return this.http.get<Hero[]>(url)
+        .pipe(
+          map(heroes => heroes[0]), // returns a {0|1} element array
+          tap(h => {
+            const outcome = h ? 'fetched' : 'did not find';
+           this.log(`${outcome} hero id=${id}`);
+          }),
+          catchError(this.handleError<Hero>(`getHero id=${id}`))
+       );
+   }
 
 
   /** GET hero by id. Will 404 if id not found */
